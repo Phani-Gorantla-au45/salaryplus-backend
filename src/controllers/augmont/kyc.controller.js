f248c04294df3a78394c771c1f85a80cf13a9944
Aitipamula Anil <aitipamulaanil@gmail.com>
2026-02-04 12:48:39 +0530

    added headers

--- src/controllers/augmont/kyc.controller.js
+++ src/controllers/augmont/kyc.controller.js
@@ -14,10 +14,10 @@ export const verifyPan = async (req, res) => {
       { pan: pan.toUpperCase() },
       {
         headers: {
+          "x-client-id": process.env.CASHFREE_PAN_CLIENT_ID,
+          "x-client-secret": process.env.CASHFREE_PAN_CLIENT_SECRET,
+          "x-api-version": "2022-10-26",
           "Content-Type": "application/json",
-          "x-client-id": process.env.CASHFREE_CLIENT_ID,
-          "x-client-secret": process.env.CASHFREE_SECRET_KEY,
-          "x-api-version": "2022-09-12",
         },
       },
     );
